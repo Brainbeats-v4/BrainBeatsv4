bfe9669de0b22de0f61b98c1c1a77f4744e8d283
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2023-02-01 17:02:40 -0500

    adds working disconnection

---
+++ frontend/src/util/Interfaces.tsx
@@ -0,0 +1,11 @@
+export interface DataStream {
+    channel00:number;
+    channel01:number;
+    channel02:number;
+    channel03:number;
+    channel04:number;
+    channel05:number;
+    channel06:number;
+    channel07:number;
+    timeStamp:number;
+}
