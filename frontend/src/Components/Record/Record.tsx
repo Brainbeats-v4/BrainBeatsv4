8e203220135da1e784d1ce23ea1898e93d8cbfc7
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2023-02-01 14:43:46 -0500

    adds some broken connectivity logic

--- frontend/src/Components/Record/Record.tsx
+++ frontend/src/Components/Record/Record.tsx
@@ -1,30 +1,39 @@
 import { Devices, initDevice } from "device-decoder";
+import { useEffect, useState } from "react";
 
 function Record() {
+    const [disconnectFlag, setDisconnectFlag] = useState(false);
+
     
-    /* This function will take input in from the device, for now it is received as input into the function
-        but we will change this into taking in the input from the settings components */
-    function initializeConnection (deviceName:string) {
-        var device;
-        if(deviceName==='cyton') {
-            device = Devices['USB'][deviceName];
-        }
-    
-        /*  Creates a stream of EEG data from the headband, there are other customizable options referrenced by the
-            documentation for device-decoder, but for our purposes we leave them out */
-        var deviceReader = initDevice(device, {
-            ondecoded: (data) => {handleChannels(data)}, // this pushes the data from the headband as it is received from the board into the channels array
+    /* This function just tells the device to begin storing input, the connection should initialize on page load */
+    async function initializeConnection () {
+        var deviceReader = await initDevice(Devices['USB']['cyton'],
+        {
+            ondecoded: (data) => {
+                if(!disconnectFlag) {
+                    handleChannels(data)
+                }
+                else if(disconnectFlag) {
+                    deviceReader.disconnect();
+                }
+            }, // this pushes the data from the headband as it is received from the board into the channels array
             onconnect: (deviceInfo) => console.log(deviceInfo), // on connection this will print out the information our device contains
-            ondisconnect: (deviceInfo) => console.log(deviceInfo) // will do the same as above but on disconnect
+            ondisconnect: (deviceInfo) => console.log(deviceInfo)
         });
-    };
+    }
 
     function handleChannels(data:any) {
         console.log(data);
     }
 
+    function stopRecording() {
+        setDisconnectFlag(true);
+    }
+
+
     return(<div>
-        <button onClick={() => initializeConnection('cyton')}>Connect Device</button>
+        <button onClick={initializeConnection}> Device</button>
+        <button onClick={stopRecording}>Disconnect Device</button>
     </div>)
 }
 
