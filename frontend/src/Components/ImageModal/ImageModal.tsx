721b5770c56b76988f44add41a0d6a3db1c44573
Aidan Fleming <aidanf2020@gmail.com>
2023-02-09 13:43:21 -0500

    Image Search API calls working

--- frontend/src/Components/ImageModal/ImageModal.tsx
+++ frontend/src/Components/ImageModal/ImageModal.tsx
@@ -1,10 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Modal } from 'react-bootstrap';
+import React, { useState } from "react";
+import { createApi } from 'unsplash-js';
 
 // Import CSS
 import './ImageModal.css';
 
 const ImageModal = () => {
+    const unsplash = createApi({ accessKey: 'qzTKf-iacxYgt-zPYxlvgbSErhKe3_u7-vbT9fmtInk' });
+    const [searchTerm, setSearchTerm] = useState("");
+    const [pics, setPics] = useState([]);
+
+    useEffect(() => {
+        unsplash.search.getPhotos({ query: searchTerm, perPage: 30, orientation: 'squarish' }).then(result => {
+            if (result.errors) {
+                // handle error here
+                console.log('error occurred: ', result.errors[0]);
+            } else {
+                const feed = result.response;
+
+                // extract total and results array from response
+                const { total, results } = feed;
+
+                // handle success here
+                console.log(`received ${results.length} photos out of ${total}`);
+                console.log('first photo: ', results[0]);
+                setPics(results)
+            }
+        });
+        }, [searchTerm]);
 
   return (
     <>
@@ -22,13 +46,21 @@ const ImageModal = () => {
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
-                // onChange={(e) => setQuery(e.target.value)}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 />
-                {/* <Button className="buttonStyle" onClick={searchFuntion}>
-                Search
-                </Button> */}
             </div>
           </Modal.Body>
+            <div className="card-list">
+                {pics.map((pic) =>
+                    <div className="card" key={pic.id}>
+                        <img
+                            className="card--image"
+                            src={pic.urls.full}
+                            width="50%"
+                            height="50%"
+                        ></img>
+                    </div>)};
+            </div>
           <Modal.Footer className='modal-container2'>
             Image Footer
           </Modal.Footer>
