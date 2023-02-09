7fd012bf33dc87cea2c3efd60324a5c5615ca15d
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-02-09 14:12:43 -0500

    Images displaying in Image Search Modal

--- frontend/src/Components/ImageModal/ImageModal.tsx
+++ frontend/src/Components/ImageModal/ImageModal.tsx
@@ -12,7 +12,7 @@ const ImageModal = () => {
     const [pics, setPics] = useState<any[]>([])
 
     function search() {
-      unsplash.search.getPhotos({ query: searchTerm, perPage: 30, orientation: 'squarish' }).then(result => {
+      unsplash.search.getPhotos({ query: searchTerm, perPage: 16, orientation: 'squarish' }).then(result => {
         if (result.errors) {
           // handle error here
           console.log('error occurred: ', result.errors[0]);
@@ -30,6 +30,11 @@ const ImageModal = () => {
       });
     }
 
+  // function selectImage(imageURL: String)
+  // {
+  //   var backImage = imageURL;
+  // }
+
   return (
     <>
       <div>
@@ -37,7 +42,9 @@ const ImageModal = () => {
           <Modal.Header className='modal-container0' closeButton>
           </Modal.Header>
           <Modal.Body className='modal-container1'>
-            <div className="searchbar-div">
+            <div id='image-search-body'>
+              <h2 id='searchTitle'>Search for an Image</h2>
+              <div className="searchbar-div">
                 <FontAwesomeIcon icon={["fas", "search"]} />
                 <input
                 type="search"
@@ -48,22 +55,24 @@ const ImageModal = () => {
                 onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <button type="button" className="btn btn-secondary" onClick={()=>search()}>Search</button>
-            </div>
-            
+              </div>
+            </div>            
           </Modal.Body>
-            <div className="card-list">
-                {pics.map((pic) =>
-                    <div className="card" key={pic.id}>
-                        <img
-                            className="card--image"
-                            src={pic.urls.full}
-                            width="50%"
-                            height="50%"
-                        ></img>
-                    </div>)};
-            </div>
           <Modal.Footer className='modal-container2'>
-            Image Footer
+            <div id='image-search-footer'>
+              <h6>Image Results</h6>
+              <div className="search-image-list">
+                {pics.map((pic) =>
+                  <div className="search-image" key={pic.id} /* onClick={}*/>
+                    <img
+                      className="search--image"
+                      src={pic.urls.thumb}
+                      width="100%"
+                      height="100%"
+                    ></img>
+                  </div>)}
+              </div>
+            </div>
           </Modal.Footer>
         </div>
       </div>
