d94d87b6a19ae4edda2e7686c24b1937c9120955
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-02-09 12:51:32 -0500

    Updating Search Modal CSS

--- frontend/src/Components/ImageModal/ImageModal.tsx
+++ frontend/src/Components/ImageModal/ImageModal.tsx
@@ -11,26 +11,31 @@ const ImageModal = () => {
       <div>
         <div className='modal-background'>
           <Modal.Header className='modal-container0' closeButton>
-            Image Search
           </Modal.Header>
           <Modal.Body className='modal-container1'>
-            <div className="searchbar-div">
-                <FontAwesomeIcon icon={["fas", "search"]} />
-                <input
-                type="search"
-                id="searchbar"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                // onChange={(e) => setQuery(e.target.value)}
-                />
-                {/* <Button className="buttonStyle" onClick={searchFuntion}>
-                Search
-                </Button> */}
+            <div id='image-search-body'>
+              <h2 id="searchTitle">Search for an Image</h2>
+              <div className="searchbar-div">
+                  <FontAwesomeIcon icon={["fas", "search"]} />
+                  <input
+                  type="search"
+                  id="searchbar"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  // onChange={(e) => setQuery(e.target.value)}
+                  />
+                  {/* <Button className="buttonStyle" onClick={searchFuntion}>
+                  Search
+                  </Button> */}
+              </div>
             </div>
           </Modal.Body>
           <Modal.Footer className='modal-container2'>
-            Image Footer
+            <div id='image-search-footer'>
+              <h6>Image Results</h6>
+              {/* Populate Image Results here */}
+            </div>
           </Modal.Footer>
         </div>
       </div>
