c99198c17f325f2ae510ed1a5fef0fe97e970013
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2023-03-06 15:41:37 -0500

    fixed display of dev debug tools

---
+++ frontend/src/util/isDev.tsx
@@ -0,0 +1,5 @@
+import process from "process";
+
+const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+export default function isDev(): boolean { return development; }
