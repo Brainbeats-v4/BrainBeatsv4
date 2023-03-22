be12b829c9aafe814f2897acc8535170a9287abc
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2023-03-22 15:39:56 -0400

    set up image compression, and limited images to < 3mb. Currently does not display properly, but the compressed base 64 is valid and decodes to the correct image

--- frontend/src/util/ImageHelperFunctions.tsx
+++ frontend/src/util/ImageHelperFunctions.tsx
@@ -1,4 +1,6 @@
 import Resizer from "react-image-file-resizer";
+const max_width = 1024;
+const max_height = 1024;
 
 // public method for encoding an Uint8Array to base64
 // from: https://stackoverflow.com/questions/11089732/display-image-from-blob-using-javascript-and-websockets
@@ -28,6 +30,48 @@ export function encode (input:Uint8Array) {
                   keyStr.charAt(enc3) + keyStr.charAt(enc4);
     }
     return output;
+}
+
+// Uses a canvas object to resize an image so that it is within the max width x height
+// Borrowed from: Borrowed from: https://github.com/josefrichter/resize/blob/master/public/
+export function resizeMe(img:any) {
+  
+  var canvas = document.createElement('canvas');
+
+  var width = img.width;
+  var height = img.height;
+
+  // calculate the width and height, constraining the proportions
+  if (width > height) {
+    if (width > max_width) {
+      //height *= max_width / width;
+      height = Math.round(height *= max_width / width);
+      width = max_width;
+    }
+  } else {
+    if (height > max_height) {
+      //width *= max_height / height;
+      width = Math.round(width *= max_height / height);
+      height = max_height;
+    }
+  }
+  
+  // resize the canvas and draw the image data into it
+  canvas.width = width;
+  canvas.height = height;
+  var ctx = canvas?.getContext("2d");
+  
+  if (!ctx) {
+    console.error("Unable to resize image");
+    return;
+  }
+
+  ctx.drawImage(img, 0, 0, width, height);
+  
+  // preview.appendChild(canvas); // do the actual resized preview
+  
+  return canvas.toDataURL("image/png",0.7); // get the data from canvas as 70% JPG (can be also PNG, etc.)
+
 }
 
 export function resize (file:File) {
