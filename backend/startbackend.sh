11e3ff127376fe77b5300ad222bd9df7c925611e
Robert Perez <rickymcmuffins@gmail.com>
2023-10-30 15:56:17 -0400

    edited startbackend.sh script to also create user and create .env file

--- backend/startbackend.sh
+++ backend/startbackend.sh
@@ -21,11 +21,34 @@ if [ "$RESULT" != "brainbeats" ];
             esac
         done
 
-        mysql -e "CREATE DATABASE brainbeats"
+        sudo mysql -e "CREATE DATABASE brainbeats"
+        sudo mysql -e "CREATE USER 'brainbeats_root'@'localhost' IDENTIFIED BY ''"
+        sudo mysql -e "GRANT ALL ON brainbeats.* TO 'brainbeats_root'@'localhost'"
     else
         echo "brainbeats database found, continuing."
 fi
 
+if ! [ -f .env ]; 
+    then
+        echo ".env file not found."
+
+        while true; do
+            read -p "Allow the script to create it? (Y/n) " yn
+            case $yn in
+                [Yy]* ) echo "creating .env file"; break;;
+                [Nn]* ) echo "exiting"; exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+        
+        echo "DATABASE_URL="mysql://brainbeats_root@localhost:3306/brainbeats"" > .env
+
+    else
+        echo ".env file found, continuing."
+fi
+
+
 # Push prisma 
+echo "pushing prisma"
 sudo npx prisma db push
 sudo npm run dev
