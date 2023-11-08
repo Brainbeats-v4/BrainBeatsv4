3466b69b84bd98e19089d57275f07cb29af4b496
Robert Perez <rickymcmuffins@gmail.com>
2023-11-08 07:52:03 -0500

    added play pause button. Doesn't work yet

--- frontend/src/Components/Modals/TrackModal/TrackModal.tsx
+++ frontend/src/Components/Modals/TrackModal/TrackModal.tsx
@@ -486,6 +486,7 @@ const TrackModal: React.FC<Props> = ({track, closeModal}) => {
   }, [trackIsPlaying]);
 
   function playTrack() {
+    console.log("playing track");
     
     var midiToPlay:string = track.midi;
     var msg:string;
@@ -583,22 +584,26 @@ const TrackModal: React.FC<Props> = ({track, closeModal}) => {
               <h6 id="track-author-text">By {track.fullname} </h6>
 
 
-              {<button type="button" className="btn btn-primary" id='play-btn' onClick={playTrack}>
+              {<button type="button" className="btn btn-primary" id='download-btn'>
                 <a style={{color: "white", textDecoration: "none", display: "flex", justifyContent: "center"}} id='download-midi-btn' download={'myTrack.MID'} href={track.midi}>
                     <FontAwesomeIcon className='modal-track-icons fa-xl' id='modal-track-play-icon' icon={["fas", "download"]} />
                     <h4>Download</h4> 
                 </a>
               </button>}
-              {/* {!trackIsPlaying && <button type="button" className="btn btn-primary" id='play-btn' onClick={playTrack}>
+              {/*{!trackIsPlaying && <button type="button" className="btn btn-primary" id='play-btn' onClick={playTrack}>
                 <a style={{color: "white", textDecoration: "none", display: "flex", justifyContent: "center"}} id='download-midi-btn' download={'myTrack.MID'} href={track.midi}>
                     <FontAwesomeIcon className='modal-track-icons fa-xl' id='modal-track-play-icon' icon={["fas", "download"]} />
                     <h4>Download</h4> 
                 </a>
               </button>} */}
-              {/* {trackIsPlaying && <button type="button" className="btn btn-primary" id='play-btn' onClick={stopTrack}>
+              {!trackIsPlaying && <button type="button" className="btn btn-primary" id='play-btn' onClick={playTrack}>
                 <FontAwesomeIcon className='modal-track-icons fa-2x' id='modal-track-play-icon' icon={["fas", "play"]} />
-                <h3>Stop</h3>
-              </button>} */}
+                <h3>Play</h3>
+              </button>} 
+              {trackIsPlaying && <button type="button" className="btn btn-primary" id='play-btn' onClick={stopTrack}>
+                <FontAwesomeIcon className='modal-track-icons fa-2x' id='modal-track-play-icon' icon={["fas", "pause"]} />
+                <h3>Pause</h3>
+              </button>} 
 
               {isDev() && <h5>Upload midi<input id="track-cover-upload"  onChange={event=> {if(!event.target.files) {return} else {uploadMidi(event.target.files[0])}}} name="midi" type="file" accept='.MID, .MIDI'/></h5>}
 
